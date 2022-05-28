5fdf7de2f25281dfb09a5b7059d4059337959345
Aluno <aluno@LI.LOCAL>
2022-05-27 21:06:13 -0300

    Comentários
    Comentarios

--- Assets/Script/Flock.cs
+++ Assets/Script/Flock.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class Flock : MonoBehaviour
 {
+    // Variáveis
     public FlockManager myManager;
     float speed;
     // Start is called before the first frame update
     void Start()
     {
+        // Aleatoriza o valor da velocidade entre o minimo e o maximo recebido através do myManager
         speed = Random.Range(myManager.minSpeed, myManager.maxSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // faz a movimentação no eixo Z
         transform.Translate(0,0,Time.deltaTime * speed);
     }
 }
--- Assets/Script/FlockManager.cs
+++ Assets/Script/FlockManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class FlockManager : MonoBehaviour
 {
+    // Variáveis
     public GameObject fishPrefab;
     public GameObject fishPrefab2;
 
@@ -17,21 +18,28 @@ public class FlockManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Instancia o array
         allFish = new GameObject[numFish];
+        // For para a criação dos peixes
         for (int i = 0; i < numFish; i++)
         {
+            // aleatoriza a posição de criação e armazena na variável pos, os seus limites é baseado no vector3 swinLimits
             Vector3 pos = this.transform.position + new Vector3(Random.Range(-swinLimits.x, swinLimits.x),
                                                                 Random.Range(-swinLimits.y, swinLimits.y),
                                                                 Random.Range(-swinLimits.z, swinLimits.z));
 
+            // Caso seja par, irá instanciar um peixe e se for impar, irá instanciar outro
             if (i%2 == 1)
             {
+                // Instancia o peixe como um gameObject(cast) na posição aleatorizada
                 allFish[i] = (GameObject)Instantiate(fishPrefab, pos, Quaternion.identity);
             }
             else if(i%2 == 0)
             {
+                // Instancia o peixe como um gameObject(cast) na posição aleatorizada
                 allFish[i] = (GameObject)Instantiate(fishPrefab2, pos, Quaternion.identity);
             }
+            // Faz a referencia do myManager nos peixes instanciados
             allFish[i].GetComponent<Flock>().myManager = this;
         }
     }
